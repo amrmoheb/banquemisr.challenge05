bfc9a77555391c67647da7d7a6e8c44651450436
Amr Moheb <Amr.Moheb@mdi.com.eg>
2025-01-22 13:36:36 +0200

    init setup

---
+++ banquemisr.challenge05/banquemisr.challenge05/banquemisr_challenge05App.swift
@@ -0,0 +1,17 @@
+//
+//  banquemisr_challenge05App.swift
+//  banquemisr.challenge05
+//
+//  Created by Amr Moheb on 22/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct banquemisr_challenge05App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
